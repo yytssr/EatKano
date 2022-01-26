9106c029fc373a689c71a6f892304d406388a84c
yytssr <53852732+yytssr@users.noreply.github.com>
2022-01-26 21:41:40 +0800

    Update index.js

--- static/index.js
+++ static/index.js
@@ -348,11 +348,11 @@
             return '倒计时多了' + ((deviation_time / 1000) - 20).toFixed(2) + "s";
         }
         SubmitResults();
-        if (score <= 49) return '试着好好练一下？';
-        if (score <= 99) return 'TCL';
-        if (score <= 149) return 'TQL';
-        if (score <= 199) return '您';
-        return '人？';
+        if (score <= 49) return '试着多给他几拳？';
+        if (score <= 99) return '还得多练';
+        if (score <= 149) return '菜逼';
+        if (score <= 199) return '菜逼';
+        return '菜逼？';
     }
 
     w.toStr = function(obj) {
